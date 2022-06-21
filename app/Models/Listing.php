d3a2fd40c096c2470881870a81d4501e61d8117e
Sadaqat Ali <sadaqatali890@gmail.com>
2022-06-22 01:11:11 +0530

    all files and folders are saved

---
+++ app/Models/Listing.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'company', 'description', 'tags', 'email', 'website', 'location', 'logo'];
+
+    public function scopeFilter($query, array $filters){
+        if($filters['tag'] ?? false){
+            $query->where('tags', 'like', '%'. request('tag'). '%');
+        }
+
+        if($filters['search'] ?? false){
+            $query->where('title', 'like', '%'. request('search'). '%')
+            ->orwhere('description', 'like', '%'. request('search'). '%')
+            ->orwhere('tags', 'like', '%'. request('search'). '%');
+        }
+    }
+}
---
+++ resources/views/components/listing-card.blade.php
@@ -0,0 +1,27 @@
+@props(['listing'])
+@php
+    if($listing->logo != ''){
+        $logo = 'storage/' . $listing->logo;
+    } else {
+        $logo = 'images/no-image.png';
+    }
+@endphp
+<x-card>
+    <div class="flex">
+        <img
+            class="hidden w-48 mr-6 md:block"
+            src="{{asset($logo)}}"
+            alt=""
+        />
+        <div>
+            <h3 class="text-2xl">
+                <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+            <x-listing-tags :tags="$listing->tags" />
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+            </div>
+        </div>
+    </div>
+</x-card>
